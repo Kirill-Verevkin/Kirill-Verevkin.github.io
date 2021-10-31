84a66835a6c8a84755cca97450c5d4a32e87fed1
Kirill-Verevkin <68014749+Kirill-Verevkin@users.noreply.github.com>
2021-10-31 13:37:17 +0300

    Add files via upload

---
+++ projects/Musik Site/favorits/morgen/assets/js/musik.js
@@ -0,0 +1,38 @@
+document.querySelector('.item-1').onclick = function () {
+	let audioBlock = document.querySelector('.audio-block');
+	audioBlock.classList.remove("hidden");
+	audioBlock.src = 'musik/merin.mp3';
+	let Img = document.querySelector('.img');
+	Img.src = 'assets/img/noviymerin-morgen.png';
+
+
+
+}
+
+
+document.querySelector('.item-2').onclick = function () {
+	let audioBlock = document.querySelector('.audio-block');
+	audioBlock.classList.remove("hidden");
+	audioBlock.src = 'musik/cadillac.mp3';
+	let Img = document.querySelector('.img');
+	Img.src = 'assets/img/cadillac.png';
+
+}
+
+
+document.querySelector('.item-3').onclick = function () {
+	let audioBlock = document.querySelector('.audio-block');
+	audioBlock.classList.remove("hidden");
+	audioBlock.src = 'musik/ice.mp3';
+	let Img = document.querySelector('.img');
+	Img.src = 'assets/img/ice.png';
+
+}
+
+
+
+
+
+
+
+
